896b203c2c0eeb2466676e36a50bdcdad54cf552
JunHyuk Heo <101388279+jnhyk@users.noreply.github.com>
2023-12-26 21:06:03 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 552 ms, Memory: 261528 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -5,9 +5,12 @@ n_list = list(map(int, input().split()))
 m = int(input())
 m_list = list(map(int, input().split()))
 
-counters = Counter(n_list)
+# 첫 번째 리스트의 각 원소의 등장 횟수를 사전에 저장
+n_counts = Counter(n_list)
 
-result = [counters[num] for num in m_list]
+# 결과 리스트 초기화
+result = [n_counts[x] for x in m_list]
 
+# 결과 출력
 for r in result:
-    print(r, end = " ")
+    print(r, end=" ")
