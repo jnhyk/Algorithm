b0b517af1dfd53a6e210f2e3697c808a3636492f
JunHyuk Heo <101388279+jnhyk@users.noreply.github.com>
2023-12-30 23:51:18 +0900

    [Silver III] Title: N과 M (2), Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,9 @@
+import itertools
+
+n, m= map(int, input().split())
+arr = [i for i in range(1,n+1)]
+nPr = list(itertools.combinations(arr, m))
+for i in range(len(nPr)):
+    for j in range(m):
+        print(nPr[i][j], end = " ")
+    print()
