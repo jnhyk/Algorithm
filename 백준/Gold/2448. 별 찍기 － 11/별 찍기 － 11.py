c6d1a268ce889cff6893100ab7d70cc11d89980e
JunHyuk Heo <101388279+jnhyk@users.noreply.github.com>
2024-01-01 23:58:25 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 164 ms, Memory: 160144 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.py
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.py
@@ -1,29 +1,17 @@
+def star(n):
+    if n ==3:
+        L.append(['  *   ',' * *  ','***** '])
+        return L
+    
+    S = star(n//2)
+    L.append(list(map(lambda x: x * 2,S[-1])))
+    L[-2] = list(map(lambda x: ' '*(n//2)+x+' '*(n//2), L[-2]))
+    L[0] += L[1]
+    L.pop()
+    return L
+    
+
+
 n = int(input())
-
-graph = [[" ", " ", "*", " ", " "], [" ", "*", " ", "*", " "], ["*", "*", "*", "*", "*"]]
-
-
-def recursive(N, before):
-    after = [[" "] * (2 * 2 * N - 1) for _ in range(2 * N)]
-    for i in range(N):
-        after[i][N:N+2*N-1] = before[i]
-
-    k = 0
-    for i in range(N, 2 * N):
-        after[i][:2*N] = before[k]
-        after[i][2 * N:2 * N+len(before[k])] = before[k]
-        k += 1
-
-    if 2 * N == n:
-        return after
-
-    return recursive(2 * N, after)
-
-
-if n == 3:
-    result = graph
-else:
-    result = recursive(3, graph)
-
-for i in result:
-    print("".join(i))
+L = []
+print('\n'.join(sum(star(n),[])))
