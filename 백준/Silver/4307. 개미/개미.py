dc338cbf7467e61743f04451099c088bf03da28a
JunHyuk Heo <101388279+jnhyk@users.noreply.github.com>
2024-01-10 02:15:32 +0900

    [Silver I] Title: 개미, Time: 268 ms, Memory: 111576 KB -BaekjoonHub

--- 백준/Silver/4307. 개미/개미.py
+++ 백준/Silver/4307. 개미/개미.py
@@ -1,12 +1,10 @@
 T = int(input())
 for _ in range(T):
     l,n = map(int,input().split())
-    loc = []
     m = 0
     M = 0
     for _ in range(n):
         num = (int(input()))
-        loc.append(num)
         if m < min(l - num, num):
             m = min(l - num, num)
         if M < max(l - num, num):
