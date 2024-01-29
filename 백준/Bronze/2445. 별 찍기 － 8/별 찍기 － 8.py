315a4626cb330da08e70717344a3be41a94e54e8
JunHyuk Heo <101388279+jnhyk@users.noreply.github.com>
2024-01-29 21:16:16 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
@@ -0,0 +1,9 @@
+n = int(input())
+for i in range(1,n+1):
+    print("*"*i,end = "")
+    print(" "*((n-i)*2),end = "")
+    print("*"*i)
+for i in range(1,n):
+    print("*"*(n-i),end = "")
+    print(" "*(i*2),end = "")
+    print("*"*(n-i))
