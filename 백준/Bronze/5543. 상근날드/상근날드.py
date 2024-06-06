feb9f2bf00811c5a04c3edead2e409d22d157b93
JunHyuk Heo <101388279+jnhyk@users.noreply.github.com>
2024-06-06 18:23:36 +0900

    [Bronze IV] Title: 상근날드, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/5543. 상근날드/상근날드.py
@@ -0,0 +1,9 @@
+burger = []
+drink = []
+
+for i in range(5):
+    if i < 3:
+        burger.append(int(input()))
+    else:
+        drink.append(int(input()))
+print(min(burger) + min(drink) - 50)
