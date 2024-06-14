d75ce3c8838d736c31ad8ec9e1283173d6083237
JunHyuk Heo <101388279+jnhyk@users.noreply.github.com>
2024-06-14 11:36:14 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.18 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,16 +1,8 @@
-a = [0,0]
-def trans(s):
-    if s == "1":
-        return
-    l1 = len(s)
-    s = s.replace("0", "")
-    l2 = len(s)
-    a[1] += l1 - l2
-    s = bin(l2)[2:]
-    a[0] += 1
-    trans(s)
-    
 def solution(s):
-    trans(s)
-    answer = a
-    return answer
+    a, b = 0, 0
+    while s != '1':
+        a += 1
+        num = s.count('1')
+        b += len(s) - num
+        s = bin(num)[2:]
+    return [a, b]
