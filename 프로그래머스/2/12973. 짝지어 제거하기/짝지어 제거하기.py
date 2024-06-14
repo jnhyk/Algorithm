17c82d372ac7448f1c63436476f551971f7ccfee
JunHyuk Heo <101388279+jnhyk@users.noreply.github.com>
2024-06-15 04:22:47 +0900

    [level 2] Title: 짝지어 제거하기, Time: 172.90 ms, Memory: 14.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = 0
+    stack = []
+    for i in s:
+        if not stack:
+            stack.append(i)
+        else:
+            if stack[-1] == i:
+                stack.pop()
+            else:
+                stack.append(i)
+    if not stack:
+        answer = 1
+    return answer
