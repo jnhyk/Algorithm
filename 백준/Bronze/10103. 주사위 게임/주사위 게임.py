4db3ca97f5758e582e3632f3585d0dc898466dc4
JunHyuk Heo <101388279+jnhyk@users.noreply.github.com>
2024-06-27 18:29:18 +0900

    [Bronze III] Title: 주사위 게임, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/10103. 주사위 게임/주사위 게임.py
+++ 백준/Bronze/10103. 주사위 게임/주사위 게임.py
@@ -3,11 +3,9 @@ a = 100
 b = 100
 for _ in range(n):
     a_dice, b_dice = map(int,input().split())
-    if a_dice == b_dice:
-        continue
-    elif a_dice < b_dice:
+    if a_dice < b_dice:
         a -= b_dice
-    else:
+    elif a_dice > b_dice:
         b -= a_dice
 print(a)
 print(b)
