f978d2cfc77874e876e6091cb1a2ffb3cfc06eea
JunHyuk Heo <101388279+jnhyk@users.noreply.github.com>
2024-07-11 18:57:41 +0900

    [Bronze III] Title: 시간과 날짜, Time: 440 ms, Memory: 112420 KB -BaekjoonHub

--- 백준/Bronze/25024. 시간과 날짜/시간과 날짜.py
+++ 백준/Bronze/25024. 시간과 날짜/시간과 날짜.py
@@ -1,7 +1,7 @@
 t = int(input())
 for _ in range(t):
-    time = "Yes"
-    day = "Yes"
+    time = "Yes" #시간이 되나!
+    day = "Yes" # 날짜가 되나!
     x, y = map(int, input().split())
     if x > 23 or y > 59:
         time = "No"
@@ -9,6 +9,6 @@ for _ in range(t):
     elif (x < 1 or x > 12) or (y < 1 or y > 31):
         day = "No"
     else:
-        if ((x == 4 or x == 6 or x == 9 or x == 11) and y > 30) or (x == 2 and y > 29):
+        if (x in [4,6,9,11] and y > 30) or (x == 2 and y > 29):
             day = "No"
     print(time, day)
