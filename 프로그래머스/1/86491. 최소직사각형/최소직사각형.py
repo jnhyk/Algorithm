54559abf927c64a275cd3ee400118c1d50f66066
jnhyk <101388279+jnhyk@users.noreply.github.com>
2024-11-02 16:17:36 +0900

    [level 1] Title: 최소직사각형, Time: 5.89 ms, Memory: 12.3 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -4,10 +4,7 @@ def solution(sizes):
     m_x = 0
     m_y = 0
     for x,y in s1:
-        if x > m_x:
-            m_x = x
-        if y > m_y:
-            m_y = y
-    
+        m_x = max(m_x, x)
+        m_y = max(m_y, y)
     answer = m_x * m_y
     return answer
