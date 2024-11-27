0617065634b09ba691d4fb8edbb02cbb2b952db8
jnhyk <101388279+jnhyk@users.noreply.github.com>
2024-11-27 21:02:55 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -1,8 +1,7 @@
 def solution(n):
     answer = 0
-    n = str(n)
-    for i in n: 
-        answer += int(i)  
+    n = str(n) 
+    for i in n:
+        answer += int(i)
+        
     return answer
-
-
