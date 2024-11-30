11d7f3a81a2876bee48ebefcff5fc2b9ad4aba05
jnhyk <101388279+jnhyk@users.noreply.github.com>
2024-11-30 14:53:27 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.c
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.c
@@ -4,13 +4,12 @@
 
 long long solution(int a, int b) {
     long long answer = 0;
-    if (a > b){
-        int temp = 0;
-        temp = a;
+    if(a > b){
+        int temp = a;
         a = b;
         b = temp;
     }
-    for(int i = a;i <=b;i++){
+    for(int i = a; i <= b;i++){
         answer += i;
     }
     return answer;
