171fd538cbfbac09ea106bdf6d01fa95f5de680c
jnhyk <101388279+jnhyk@users.noreply.github.com>
2024-12-05 19:35:23 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -1,14 +1,6 @@
 def solution(num_list):
-    answer = 0
-    even = 0
-    odd = 0
-    for i in range(len(num_list)):
-        if i % 2 == 0:
-            odd += num_list[i]
-        else:
-            even += num_list[i]
-    if odd > even:
-        answer = odd
-    else:
-        answer = even
-    return answer
+    e = sum(num_list[i] for i in range(0,len(num_list),2))
+    o = sum(num_list[i] for i in range(1,len(num_list),2))
+    return max(e,o)
+
+    
